0d00b7038af352cdd6150ded6fa4e999d5955b5a
Lekxy-dev <pablotech2007@gmail.com>
2024-11-22 09:37:08 -0800

    initial commit

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -1,17 +1,32 @@
-import bcrypt from "bcrypt"
+import { NextResponse } from "next/server";
 import prisma from '@/libs/prismadb'
-import { NextResponse } from "next/server"
-
-export async function POST(request: Request){
-    const body = await request.json()
-    const {name,email,password} = body
-
-    const hashedPassword = await bcrypt.hash(password, 10)
-
-    const user = await prisma.user.create({
-        data:{
-            name, email, hashedPassword
-        }
-    })
-    return NextResponse.json(user);
-}
+import bcrypt from 'bcrypt'
+export async function POST(request: Request) {
+    try {
+      const body = await request.json();
+      const { name, email, password } = body;
+  
+      if (!name || !email || !password) {
+        return NextResponse.json(
+          { error: "All fields are required." },
+          { status: 400 }
+        );
+      }
+  
+      // Hash password and save user
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const user = await prisma.user.create({
+        data: {
+          name,
+          email,
+          hashedPassword,
+        },
+      });
+  
+      return NextResponse.json(user);
+    } catch (error) {
+      console.error("Error in /api/register:", error);
+      return NextResponse.json({ error: "Internal server error." }, { status: 500 });
+    }
+  }
+  
