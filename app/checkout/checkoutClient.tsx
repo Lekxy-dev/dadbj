624623815887e3116ace9e921a3cc824d08c9f2d
Lekxy-dev <pablotech2007@gmail.com>
2024-11-28 09:37:29 -0800

    initial commit

--- app/checkout/checkoutClient.tsx
+++ app/checkout/checkoutClient.tsx
@@ -1,14 +1,21 @@
 "use client";
 
 import { useCart } from "@/hooks/useCart";
+import { loadStripe, StripeElementsOptions } from "@stripe/stripe-js";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
+
+
+const stripePromise = loadStripe(
+  process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY as string
+)
 
 const CheckoutClient = () => {
   const { cartPs, paymentIntent, handleSetPaymentIntent } = useCart();
   const [loading, setLoading] = useState(false);
   const [clientSecret, setClientSecret] = useState("");
-
+ const [error, setError] = useState(false)
   const router = useRouter();
   console.log("payint",paymentIntent)
   console.log("clientsec",clientSecret)
@@ -25,39 +32,32 @@ const CheckoutClient = () => {
         }),
       })
         .then((res) => {
-          // Check if response is OK (status 200-299)
-          if (!res.ok) {
-            throw new Error(`API error: ${res.status}`);
+          setLoading(false);
+          if(res.status === 401){
+             router.push('/login')
           }
-
-          return res.text(); // First, get the response as text to inspect it
-        })
-        .then((text) => {
-          if (!text) {
-            throw new Error("Empty response body from API.");
-          }
-
-          // Try parsing the response as JSON
-          const data = JSON.parse(text);
-
-          // Handle the client secret if it's returned
-          if (data.paymentIntent?.client_secret) {
+          return res.json()
+        }).then((data) =>{
             setClientSecret(data.paymentIntent.client_secret);
             handleSetPaymentIntent(data.paymentIntent.id);
-          } else {
-            throw new Error("Invalid response structure. Missing client_secret.");
-          }
         })
         .catch((error) => {
-          console.error("Error in checkout process:", error);
-          // Optionally, show a toast or alert to the user
+          setError(true);
+          console.log("Error", error)
+          toast.error("something went wrong")
         })
         .finally(() => {
           setLoading(false);
         });
     }
   }, [cartPs, paymentIntent, handleSetPaymentIntent]);
-
+       const options: StripeElementsOptions = {
+                 clientSecret,
+                 appearance:{
+                 theme: "stripe",
+                 labels:'floating'
+                 }
+       }
   return (
     <div>
       <h1>Checkout</h1>
